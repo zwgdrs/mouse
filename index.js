0ebfdc0aff321a40255490977844a0ab0d5abdb8
zhuwei <zhuwei2015@ipalfish.com>
2019-11-09 12:06:22 +0800

    打地鼠组件

---
+++ index.js
@@ -0,0 +1,14 @@
+import client from './src/client'
+import editor from './src/editor'
+import attr from './src/config/attr'
+import init from './src/config/init'
+import preload from './src/config/preload'
+import verify from './src/config/verify'
+export {
+    client,
+    editor,
+    attr,
+    init,
+    preload,
+    verify,
+}
